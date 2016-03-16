8232218ddae41b40c114e8cd18f68547fdc3ba4e
oscar <46996403c@iespoblenou.org>
2016-03-16 18:06:29 +0100

    Creo las colisiones del ufo con las paredes

--- main.js
+++ main.js
@@ -31,35 +31,43 @@ var mainState = (function (_super) {
     };
     mainState.prototype.create = function () {
         _super.prototype.create.call(this);
-        //var background;
+        this.createWalls();
+        this.createPlayer();
+        this.cursor = this.input.keyboard.createCursorKeys();
+    };
+    mainState.prototype.createWalls = function () {
         this.walls = this.add.group();
         this.walls.enableBody = true;
         var wall_up = this.add.sprite(0, 0, 'up', null, this.walls);
         var wall_left = this.add.sprite(0, wall_up.height, 'left', null, this.walls);
         /*
-        background = this.add.sprite(0, 0, 'background');
-        var scale = this.world.height / background.height;
-        background.scale.setTo(scale, scale);
-        */
+         background = this.add.sprite(0, 0, 'background');
+         var scale = this.world.height / background.height;
+         background.scale.setTo(scale, scale);
+         */
         var center = this.add.sprite(wall_left.width, wall_up.height, 'center', null);
         var wall_right = this.add.sprite(wall_left.width + center.width, wall_up.height, 'right', null, this.walls);
         var wall_down = this.add.sprite(0, wall_up.height + center.height, 'down', null, this.walls);
+    };
+    ;
+    mainState.prototype.createPlayer = function () {
         this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
         //this.ufo.scale.setTo(scale - 0.05, scale - 0.05);
         //this.ufo.width = this.ufo.height = this.UFO_SIZE;
         this.ufo.anchor.setTo(0.5, 0.5);
         this.physics.enable(this.ufo, Physics.ARCADE);
         this.ufo.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); //coordenadas X, Y
-        this.cursor = this.input.keyboard.createCursorKeys();
         this.ufo.body.collideWorldBounds = true;
         this.ufo.body.bounce.set(0.8); //Valor entre 0.0 y 1.0
         this.ufo.body.drag.setTo(this.DRAG, this.DRAG); //x,y
         this.ufo.body.angularDrag = this.maxAcceleracionAngular;
     };
+    ;
     mainState.prototype.update = function () {
         _super.prototype.update.call(this);
         this.game.debug.bodyInfo(this.ufo, 0, 0);
         this.moverUFO();
+        this.physics.arcade.collide(this.ufo, this.walls);
     };
     mainState.prototype.moverUFO = function () {
         //Quitamos la velocidad por acceleracion, de forma que el movimiento sea mas natural
--- main.ts
+++ main.ts
@@ -30,21 +30,28 @@ class mainState extends Phaser.State {
 
     create():void {
         super.create();
-        //var background;
+        this.createWalls();
+        this.createPlayer();
+        this.cursor = this.input.keyboard.createCursorKeys();
+    }
+
+    private createWalls() {
         this.walls = this.add.group();
         this.walls.enableBody = true;
 
         var wall_up = this.add.sprite(0, 0, 'up', null, this.walls);
         var wall_left = this.add.sprite(0, wall_up.height, 'left', null, this.walls);
         /*
-        background = this.add.sprite(0, 0, 'background');
-        var scale = this.world.height / background.height;
-        background.scale.setTo(scale, scale);
-        */
+         background = this.add.sprite(0, 0, 'background');
+         var scale = this.world.height / background.height;
+         background.scale.setTo(scale, scale);
+         */
         var center = this.add.sprite(wall_left.width, wall_up.height, 'center', null);
         var wall_right = this.add.sprite(wall_left.width + center.width, wall_up.height, 'right', null, this.walls);
         var wall_down = this.add.sprite(0, wall_up.height + center.height, 'down', null, this.walls);
+    };
 
+    private createPlayer(){
         this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
         //this.ufo.scale.setTo(scale - 0.05, scale - 0.05);
         //this.ufo.width = this.ufo.height = this.UFO_SIZE;
@@ -52,17 +59,18 @@ class mainState extends Phaser.State {
 
         this.physics.enable(this.ufo, Physics.ARCADE);
         this.ufo.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); //coordenadas X, Y
-        this.cursor = this.input.keyboard.createCursorKeys()
+
         this.ufo.body.collideWorldBounds = true;
         this.ufo.body.bounce.set(0.8); //Valor entre 0.0 y 1.0
         this.ufo.body.drag.setTo(this.DRAG, this.DRAG); //x,y
         this.ufo.body.angularDrag = this.maxAcceleracionAngular;
-    }
+    };
 
     update():void {
         super.update();
         this.game.debug.bodyInfo(this.ufo, 0, 0);
         this.moverUFO();
+        this.physics.arcade.collide(this.ufo, this.walls);
     }
 
     private moverUFO() {
