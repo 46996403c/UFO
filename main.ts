6eb01b8b2ec18ffdda405a1bcc6b6bcfac63dc94
oscar <46996403c@iespoblenou.org>
2016-03-16 17:58:12 +0100

    Añado las paredes que limitaran el movimiento del UFO

--- main.ts
+++ main.ts
@@ -12,28 +12,42 @@ class mainState extends Phaser.State {
     private DRAG = 200; // pixels/second
     private acceleracionAngular = 50;
     private maxAcceleracionAngular = 100;
+    private walls:Phaser.Group;
 
     preload():void {
         super.preload();
 
-        this.load.image('ufo', 'assets/UFO.png');
+        this.load.image('ufo', 'assets/UFO_small.png');
         this.load.image('pickup', 'assets/Pickup.png');
         this.load.image('background', 'assets/Background.png');
-
+        this.load.image('center', 'assets/center.png');
+        this.load.image('up', 'assets/up.png');
+        this.load.image('down', 'assets/down.png');
+        this.load.image('left', 'assets/left.png');
+        this.load.image('right', 'assets/right.png');
         this.physics.startSystem(Physics.ARCADE);
     }
 
     create():void {
         super.create();
-        var background;
+        //var background;
+        this.walls = this.add.group();
+        this.walls.enableBody = true;
 
+        var wall_up = this.add.sprite(0, 0, 'up', null, this.walls);
+        var wall_left = this.add.sprite(0, wall_up.height, 'left', null, this.walls);
+        /*
         background = this.add.sprite(0, 0, 'background');
         var scale = this.world.height / background.height;
         background.scale.setTo(scale, scale);
+        */
+        var center = this.add.sprite(wall_left.width, wall_up.height, 'center', null);
+        var wall_right = this.add.sprite(wall_left.width + center.width, wall_up.height, 'right', null, this.walls);
+        var wall_down = this.add.sprite(0, wall_up.height + center.height, 'down', null, this.walls);
 
         this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
         //this.ufo.scale.setTo(scale - 0.05, scale - 0.05);
-        this.ufo.width = this.ufo.height = this.UFO_SIZE;
+        //this.ufo.width = this.ufo.height = this.UFO_SIZE;
         this.ufo.anchor.setTo(0.5, 0.5);
 
         this.physics.enable(this.ufo, Physics.ARCADE);
